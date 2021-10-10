039d66b2b87aae708b65d9b92485f9dec9d451ad
RayceWheat <RayceWheat@gmail.com>
2021-10-09 22:09:28 -0500

    added file writing

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
 
 const questions = [
@@ -31,13 +34,97 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'installtion',
+        name: 'installation',
         message: 'Please explain how to install your project',
         validate: installationInput => {
             if (installationInput) {
                 return true;
             } else {
-                console.log('You must describe ')
+                console.log('You must describe how to install your project.')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please explain how to use your project',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You must explain how to you your project.')
+                return false;
+            }
+
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: [
+            'Apache license 2.0',
+            'MIT License',
+            'GNU General Public License v3.0',
+        ],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('You must select a license')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please input contribution guide lines',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please input contribution guide lines');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please explain your tests',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please input test explanation');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please input your Github username for potential questions',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('You must input your Github username');
+                return false;
+            }
+
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please input an email to be contacted',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('You must input your email');
+                return false;
             }
         }
     }
@@ -57,15 +144,58 @@ Questions
 
 const deconstructInput = (projectData) => {
     console.log(projectData);
-    const { title, description, installation} = projectData;
+    const { title, description, installation,  usage, contribution, license, github, email} = projectData;
 
-    return`
+    console.log(license);
 
-    # ${title}
-    
-    --
-    `;
+return`# ${title}
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Description
+${description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [License](#license)
+
+## Installation 
+${installation}
+
+## Usage
+${usage}
+
+## Contribution
+${contribution}
+
+## License
+${license}
+
+## Questions
+Questions may be directred at either ${github} or ${email}. 
+`;
 }
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('../dist/README.md', fileContent, err => {
+            // if there's an error reject promise and send error to the Promise's 'catch'
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promse
+                return;
+            }
+
+            // if everything went well, resolve the Promise
+            resolve({
+                ok: true,
+                message: 'File created'
+            });
+        });
+    });
+};
 
 const promptUser = () => {
     return inquirer.prompt(questions)
@@ -74,9 +204,13 @@ const promptUser = () => {
 promptUser()
 .then((projectData) => {
     return deconstructInput(projectData);
+})
+.then(markdownData => {
+    return writeFile(markdownData);
 });
 
 
+/*
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -86,3 +220,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+*/
---
+++ dist/README.md
@@ -0,0 +1,27 @@
+# Test
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Description
+Test
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [License](#license)
+
+## Installation 
+Test
+
+## Usage
+Test
+
+## Contribution
+Test
+
+## License
+Apache license 2.0
+
+##Questions
+Questions may be directred at either Test or Test. 
